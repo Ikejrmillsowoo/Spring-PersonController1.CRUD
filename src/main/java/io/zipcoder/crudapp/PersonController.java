089b8652f711e7414f402354ff439b8f71f6f43d
Isaac Mills-Owoo <ikejrmillsowoo@gmail.com>
2025-09-09 01:10:38 -0400

    spring practice

---
+++ src/main/java/io/zipcoder/crudapp/PersonController.java
@@ -0,0 +1,42 @@
+package io.zipcoder.crudapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/people")
+public class PersonController {
+    @Autowired
+    PersonRepository repository;
+
+    @PostMapping
+    Person createPerson(@RequestBody Person p){
+        Person newPerson = repository.save(p);
+        return newPerson;
+    }
+    @GetMapping("/{id}")
+    Person getPerson(@PathVariable int id){
+       return repository.findOne(id);
+    }
+    @GetMapping
+    List<Person> getPersonList(){
+        return (List<Person>) repository.findAll();
+    }
+    @PutMapping("/{id}")
+    Person updatePerson(@RequestBody Person p){
+       Person oldPerson =  getPerson(p.getId());
+       oldPerson.setFirstName(p.getFirstName());
+       oldPerson.setLastName(p.getLastName());
+        return oldPerson;
+    }
+
+    @DeleteMapping("/{id}")
+    void deletePerson(@PathVariable int id){
+    Person personToDelete = getPerson(id);
+    repository.delete(personToDelete);
+    }
+
+
+}
